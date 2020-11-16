d22a5be650cdddcff5f61aa570bb9ca3a2f76f79
ChampPK <thechamp2535@gmail.com>
2020-11-16 22:55:28 +0700

    create project express

---
+++ data.js
@@ -0,0 +1,21 @@
+const data = [
+  {
+    url: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4",
+    channel: "sssOne",
+    description: "Learn the MERN Stack - Full Tutorial for Beginners",
+    song: "Take on me",
+    likes: 111,
+    messages: 200,
+    shares: 20
+  }, {
+    url: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4",
+    channel: "ssssTow",
+    description: "React Material ready to use from the official website",
+    song: "Black in black",
+    likes: 200,
+    messages: 500,
+    shares: 100
+  }
+]
+
+export default data
